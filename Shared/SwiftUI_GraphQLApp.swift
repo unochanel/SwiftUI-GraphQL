f1850b28763ecd0709d0089135a82a132427197e
unochanel <shanksryouop@gmail.com>
2021-05-09 21:43:05 +0900

    Initial Commit

---
+++ Shared/SwiftUI_GraphQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GraphQLApp.swift
+//  Shared
+//
+//  Created by 宇野 凌平 on 2021/05/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GraphQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
