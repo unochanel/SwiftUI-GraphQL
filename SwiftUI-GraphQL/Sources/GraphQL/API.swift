7838926b06a4c52ec0e3dd26a90cf9edc10d0f99
unochanel <shanksryouop@gmail.com>
2021-05-12 00:22:05 +0900

    fix query

--- SwiftUI-GraphQL/Sources/GraphQL/API.swift
+++ SwiftUI-GraphQL/Sources/GraphQL/API.swift
@@ -4,12 +4,61 @@
 import Apollo
 import Foundation
 
+/// Represents the individual results of a search.
+public enum SearchType: RawRepresentable, Equatable, Hashable, CaseIterable, Apollo.JSONDecodable, Apollo.JSONEncodable {
+  public typealias RawValue = String
+  /// Returns results matching issues in repositories.
+  case issue
+  /// Returns results matching repositories.
+  case repository
+  /// Returns results matching users and organizations on GitHub.
+  case user
+  /// Auto generated constant for unknown enum values
+  case __unknown(RawValue)
+
+  public init?(rawValue: RawValue) {
+    switch rawValue {
+      case "ISSUE": self = .issue
+      case "REPOSITORY": self = .repository
+      case "USER": self = .user
+      default: self = .__unknown(rawValue)
+    }
+  }
+
+  public var rawValue: RawValue {
+    switch self {
+      case .issue: return "ISSUE"
+      case .repository: return "REPOSITORY"
+      case .user: return "USER"
+      case .__unknown(let value): return value
+    }
+  }
+
+  public static func == (lhs: SearchType, rhs: SearchType) -> Bool {
+    switch (lhs, rhs) {
+      case (.issue, .issue): return true
+      case (.repository, .repository): return true
+      case (.user, .user): return true
+      case (.__unknown(let lhsValue), .__unknown(let rhsValue)): return lhsValue == rhsValue
+      default: return false
+    }
+  }
+
+  public static var allCases: [SearchType] {
+    return [
+      .issue,
+      .repository,
+      .user,
+    ]
+  }
+}
+
 public final class RepositoryQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition: String =
     """
-    query Repository($first: Int!, $query: String!) {
-      search(first: $first, query: $query, type: Repositpry) {
+    query Repository($first: Int!, $query: String!, $type: SearchType!) {
+      search(first: $first, query: $query, type: $type) {
         __typename
         repositoryCount
         nodes {
@@ -27,14 +76,16 @@ public final class RepositoryQuery: GraphQLQuery {
 
   public var first: Int
   public var query: String
+  public var type: SearchType
 
-  public init(first: Int, query: String) {
+  public init(first: Int, query: String, type: SearchType) {
     self.first = first
     self.query = query
+    self.type = type
   }
 
   public var variables: GraphQLMap? {
-    return ["first": first, "query": query]
+    return ["first": first, "query": query, "type": type]
   }
 
   public struct Data: GraphQLSelectionSet {
@@ -42,7 +93,7 @@ public final class RepositoryQuery: GraphQLQuery {
 
     public static var selections: [GraphQLSelection] {
       return [
-        GraphQLField("search", arguments: ["first": GraphQLVariable("first"), "query": GraphQLVariable("query"), "type": "Repositpry"], type: .nonNull(.object(Search.selections))),
+        GraphQLField("search", arguments: ["first": GraphQLVariable("first"), "query": GraphQLVariable("query"), "type": GraphQLVariable("type")], type: .nonNull(.object(Search.selections))),
       ]
     }
 
