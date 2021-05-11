8e268a3dacb481141793b7ef39864f10a7ddfbd7
misakiagata <agata-misaki@cyber-z.co.jp>
2021-05-11 22:28:31 +0900

    Add: DetailRepositoryView.swift

---
+++ SwiftUI-GraphQL/Sources/Detail/DetailRepositoryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DetailRepositoryView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+struct DetailRepositoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailRepositoryView()
+    }
+}
