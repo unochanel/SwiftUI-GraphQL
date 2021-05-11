6d0de40f71a2d633397ced54926c782c8bc31a55
unochanel <shanksryouop@gmail.com>
2021-05-12 00:03:29 +0900

    generate query for repository

--- SwiftUI-GraphQL/Sources/GraphQL/API.swift
+++ SwiftUI-GraphQL/Sources/GraphQL/API.swift
@@ -4,65 +4,20 @@
 import Apollo
 import Foundation
 
-/// Represents the individual results of a search.
-public enum SearchType: RawRepresentable, Equatable, Hashable, CaseIterable, Apollo.JSONDecodable, Apollo.JSONEncodable {
-  public typealias RawValue = String
-  /// Returns results matching issues in repositories.
-  case issue
-  /// Returns results matching repositories.
-  case repository
-  /// Returns results matching users and organizations on GitHub.
-  case user
-  /// Auto generated constant for unknown enum values
-  case __unknown(RawValue)
-
-  public init?(rawValue: RawValue) {
-    switch rawValue {
-      case "ISSUE": self = .issue
-      case "REPOSITORY": self = .repository
-      case "USER": self = .user
-      default: self = .__unknown(rawValue)
-    }
-  }
-
-  public var rawValue: RawValue {
-    switch self {
-      case .issue: return "ISSUE"
-      case .repository: return "REPOSITORY"
-      case .user: return "USER"
-      case .__unknown(let value): return value
-    }
-  }
-
-  public static func == (lhs: SearchType, rhs: SearchType) -> Bool {
-    switch (lhs, rhs) {
-      case (.issue, .issue): return true
-      case (.repository, .repository): return true
-      case (.user, .user): return true
-      case (.__unknown(let lhsValue), .__unknown(let rhsValue)): return lhsValue == rhsValue
-      default: return false
-    }
-  }
-
-  public static var allCases: [SearchType] {
-    return [
-      .issue,
-      .repository,
-      .user,
-    ]
-  }
-}
-
 public final class RepositoryQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition: String =
     """
-    query Repository($first: Int!, $query: String!, $type: SearchType) {
-      search(first: $first, query: $query, type: $type) {
+    query Repository($first: Int!, $query: String!) {
+      search(first: $first, query: $query, type: Repositpry) {
         __typename
-        ... on Repository {
-          id
-          name
+        repositoryCount
+        nodes {
+          __typename
+          ... on Repository {
+            id
+            name
+          }
         }
       }
     }
@@ -72,16 +27,14 @@ public final class RepositoryQuery: GraphQLQuery {
 
   public var first: Int
   public var query: String
-  public var type: SearchType?
 
-  public init(first: Int, query: String, type: SearchType? = nil) {
+  public init(first: Int, query: String) {
     self.first = first
     self.query = query
-    self.type = type
   }
 
   public var variables: GraphQLMap? {
-    return ["first": first, "query": query, "type": type]
+    return ["first": first, "query": query]
   }
 
   public struct Data: GraphQLSelectionSet {
@@ -89,7 +42,7 @@ public final class RepositoryQuery: GraphQLQuery {
 
     public static var selections: [GraphQLSelection] {
       return [
-        GraphQLField("search", arguments: ["first": GraphQLVariable("first"), "query": GraphQLVariable("query"), "type": GraphQLVariable("type")], type: .nonNull(.object(Search.selections))),
+        GraphQLField("search", arguments: ["first": GraphQLVariable("first"), "query": GraphQLVariable("query"), "type": "Repositpry"], type: .nonNull(.object(Search.selections))),
       ]
     }
 
@@ -119,6 +72,8 @@ public final class RepositoryQuery: GraphQLQuery {
       public static var selections: [GraphQLSelection] {
         return [
           GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("repositoryCount", type: .nonNull(.scalar(Int.self))),
+          GraphQLField("nodes", type: .list(.object(Node.selections))),
         ]
       }
 
@@ -128,8 +83,8 @@ public final class RepositoryQuery: GraphQLQuery {
         self.resultMap = unsafeResultMap
       }
 
-      public init() {
-        self.init(unsafeResultMap: ["__typename": "SearchResultItemConnection"])
+      public init(repositoryCount: Int, nodes: [Node?]? = nil) {
+        self.init(unsafeResultMap: ["__typename": "SearchResultItemConnection", "repositoryCount": repositoryCount, "nodes": nodes.flatMap { (value: [Node?]) -> [ResultMap?] in value.map { (value: Node?) -> ResultMap? in value.flatMap { (value: Node) -> ResultMap in value.resultMap } } }])
       }
 
       public var __typename: String {
@@ -138,6 +93,145 @@ public final class RepositoryQuery: GraphQLQuery {
         }
         set {
           resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      /// The number of repositories that matched the search query.
+      public var repositoryCount: Int {
+        get {
+          return resultMap["repositoryCount"]! as! Int
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "repositoryCount")
+        }
+      }
+
+      /// A list of nodes.
+      public var nodes: [Node?]? {
+        get {
+          return (resultMap["nodes"] as? [ResultMap?]).flatMap { (value: [ResultMap?]) -> [Node?] in value.map { (value: ResultMap?) -> Node? in value.flatMap { (value: ResultMap) -> Node in Node(unsafeResultMap: value) } } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [Node?]) -> [ResultMap?] in value.map { (value: Node?) -> ResultMap? in value.flatMap { (value: Node) -> ResultMap in value.resultMap } } }, forKey: "nodes")
+        }
+      }
+
+      public struct Node: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["App", "Issue", "MarketplaceListing", "Organization", "PullRequest", "Repository", "User"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLTypeCase(
+              variants: ["Repository": AsRepository.selections],
+              default: [
+                GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              ]
+            )
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public static func makeApp() -> Node {
+          return Node(unsafeResultMap: ["__typename": "App"])
+        }
+
+        public static func makeIssue() -> Node {
+          return Node(unsafeResultMap: ["__typename": "Issue"])
+        }
+
+        public static func makeMarketplaceListing() -> Node {
+          return Node(unsafeResultMap: ["__typename": "MarketplaceListing"])
+        }
+
+        public static func makeOrganization() -> Node {
+          return Node(unsafeResultMap: ["__typename": "Organization"])
+        }
+
+        public static func makePullRequest() -> Node {
+          return Node(unsafeResultMap: ["__typename": "PullRequest"])
+        }
+
+        public static func makeUser() -> Node {
+          return Node(unsafeResultMap: ["__typename": "User"])
+        }
+
+        public static func makeRepository(id: GraphQLID, name: String) -> Node {
+          return Node(unsafeResultMap: ["__typename": "Repository", "id": id, "name": name])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var asRepository: AsRepository? {
+          get {
+            if !AsRepository.possibleTypes.contains(__typename) { return nil }
+            return AsRepository(unsafeResultMap: resultMap)
+          }
+          set {
+            guard let newValue = newValue else { return }
+            resultMap = newValue.resultMap
+          }
+        }
+
+        public struct AsRepository: GraphQLSelectionSet {
+          public static let possibleTypes: [String] = ["Repository"]
+
+          public static var selections: [GraphQLSelection] {
+            return [
+              GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+              GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+              GraphQLField("name", type: .nonNull(.scalar(String.self))),
+            ]
+          }
+
+          public private(set) var resultMap: ResultMap
+
+          public init(unsafeResultMap: ResultMap) {
+            self.resultMap = unsafeResultMap
+          }
+
+          public init(id: GraphQLID, name: String) {
+            self.init(unsafeResultMap: ["__typename": "Repository", "id": id, "name": name])
+          }
+
+          public var __typename: String {
+            get {
+              return resultMap["__typename"]! as! String
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "__typename")
+            }
+          }
+
+          public var id: GraphQLID {
+            get {
+              return resultMap["id"]! as! GraphQLID
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "id")
+            }
+          }
+
+          /// The name of the repository.
+          public var name: String {
+            get {
+              return resultMap["name"]! as! String
+            }
+            set {
+              resultMap.updateValue(newValue, forKey: "name")
+            }
+          }
         }
       }
     }
