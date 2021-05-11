b9bd629bcd623b6593c83d092d84b54931e78691
misakiagata <agata-misaki@cyber-z.co.jp>
2021-05-11 20:34:29 +0900

    Use: SearchRepositoryView.swift instead of ContentView.swift

---
+++ SwiftUI-GraphQL/Sources/Search/SearchRepositoryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SearchRepositoryView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SearchRepositoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchRepositoryView()
+    }
+}
